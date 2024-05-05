c65d9fb0a8e1da52c34c1692070da6705a81c81e
710lucas <lucasperonicobarbotin@gmail.com>
2024-05-05 18:39:20 -0300

    mudando estilo de pagina principal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,26 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import Heading from '@theme/Heading';
+
 import styles from './index.module.css';
+import "./index.css"
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  const Svg: React.ComponentType<React.ComponentProps<'svg'>> = require("@site/static/img/logo01-1.svg").default;
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--primary banner', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          <Svg className='logo'/>
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className="button button--secondary button--lg processo-button"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Conheça o processo!
           </Link>
         </div>
       </div>
@@ -28,14 +30,14 @@ function HomepageHeader() {
   );
 }
 
-export default function Home() {
+export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
+      <main className='features-container'>
         <HomepageFeatures />
       </main>
     </Layout>
